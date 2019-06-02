f5adb4673b1b3ac2f6997d1813ac654851dcd37c
zaboltar <cletothar@gmail.com>
2019-06-01 23:55:24 -0500

    enemy reset room

--- ARGLRPGJam/RGLRPGJam/Assets/Scripts/Enemies/LogEnemy.cs
+++ ARGLRPGJam/RGLRPGJam/Assets/Scripts/Enemies/LogEnemy.cs
@@ -41,6 +41,8 @@ public class LogEnemy : MonoBehaviour
     void OnEnable ()
     {
         transform.position = homePosition;
+        health = maxHealth.initialValue;
+        currentState = EnemyState.idle;
     }
 
     private void TakeDamage(float damage)
